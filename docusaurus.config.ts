1699f4b3759e1206e9331a58ede8d773b4098332
Mike A. <git@mikealmel.ooo>
2026-05-19 20:35:24 +0200

    update branding

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,13 +1,13 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'EDAcation',
-  tagline: 'A learning environment for digital hardware design',
-  favicon: 'img/favicon.ico',
+  title: "EDAcation",
+  tagline: "A learning environment for digital hardware design",
+  favicon: "img/logo.svg",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,36 +15,36 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://edacation.github.io',
+  url: "https://edacation.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'EDAcation', // Usually your GitHub org/user name.
-  projectName: 'EDAcation.github.io', // Usually your repo name.
+  organizationName: "EDAcation", // Usually your GitHub org/user name.
+  projectName: "vscode-edacation", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -52,69 +52,74 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'EDAcation',
+      title: "EDAcation",
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
         },
         {
-          type: 'doc',
-          docId: 'get-started/installation',
-          label: 'Get Started',
-          position: 'left',
+          type: "doc",
+          docId: "get-started/installation",
+          label: "Get Started",
+          position: "left",
         },
         {
-          type: 'doc',
-          docId: 'reference/tasks',
-          label: 'Reference',
-          position: 'left',
+          type: "doc",
+          docId: "reference/tasks",
+          label: "Reference",
+          position: "left",
         },
         {
-          href: 'https://github.com/EDAcation/EDAcation.github.io',
-          label: 'GitHub',
-          position: 'right',
+          to: "/projects",
+          label: "Projects",
+          position: "left",
+        },
+        {
+          href: "https://github.com/EDAcation/EDAcation.github.io",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Getting Started',
-              to: '/docs/get-started/installation',
+              label: "Getting Started",
+              to: "/docs/get-started/installation",
             },
             {
-              label: 'Reference',
-              to: '/docs/reference/tasks',
+              label: "Reference",
+              to: "/docs/reference/tasks",
             },
             {
-              label: 'Troubleshooting',
-              to: '/docs/troubleshooting/common-issues',
+              label: "Troubleshooting",
+              to: "/docs/troubleshooting/common-issues",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/EDAcation/EDAcation.github.io',
+              label: "GitHub",
+              href: "https://github.com/EDAcation/EDAcation.github.io",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} EDAcation. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} EDAcation. Made with ❤️ at the <a href="https://www.utwente.nl" target="_blank" rel="noopener noreferrer">University of Twente</a>.`,
     },
     prism: {
       theme: prismThemes.github,
